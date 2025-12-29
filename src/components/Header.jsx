8c4498e5703ac9726e69fa3f8633e5c35280839f
Haris Delalic <delalicharis@gmail.com>
2025-12-30 00:01:49 +0100

    add configurable button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
 import RestaurantLogo from '../assets/logo.jpg'
+import Button from "./UI/Button.jsx";
 export default function Header() {
     return <header id="main-header">
         <div id="title">
             <img src={RestaurantLogo} alt="Restaurant Logo" />
             <h1> Food Ordering App </h1>
         </div>
-        <button>Cart (0)</button>
+        <Button textOnly>Cart (0)</Button>
     </header>
 }
